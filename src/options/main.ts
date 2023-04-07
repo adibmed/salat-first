480a2ce507abbc8c60de89e60e78c9355a5a2558
Adib Mohamed <mr.eladib.mohamed@gmail.com>
2023-04-07 03:00:31 +0000

    fix: options page

--- src/options/main.ts
+++ src/options/main.ts
@@ -1,6 +1,5 @@
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import './index.css'
 
-createApp(App).use(ElementPlus).mount('#app')
+createApp(App).mount('#app')
